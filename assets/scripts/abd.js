7152fd9104a116c3f79602bcb30b968b7e1b9302
Samplasion <samplasion@gmail.com>
2019-09-27 14:37:22 +0200

    A more efficient ad block detector

---
+++ assets/scripts/abd.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    if(document.getElementById('OqZDWAsgjiLK')){
+        // alert('Blocking Ads: No');
+    } else {
+        // Adblock detected
+
+        // Create a "Please unblock us" banner
+        var aab = $("<div></div>");
+        aab.css("height", "250")
+           .css("outline", "5px solid #c44040")
+           .css("background-color", "#c44040")
+           .css("border", "5px dashed white")
+           .css("padding", "5px")
+           .css("display", "grid")
+           .css("align-items", "center")
+           .css("color", "white");
+        
+        aab.html("<h1>You're using an ad blocker</h1><p>That's ok, I also do so. But could you consider unblocking me? I promise my ads aren't invasive. :)</p>");
+        
+        $(".mapache-ad.sidebar-items > ins").hide();
+        $(".mapache-ad.sidebar-items").prepend(aab);
+
+        var banner_aab = aab.clone()
+            .css("height", "auto")
+            .css("width", "100%")
+            .css("display", "auto")
+            .css("padding", "15px");
+
+        banner_aab.html("<h3>You're using an ad blocker</h3><p>That's ok, I also do so. But could you consider unblocking me? I promise my ads aren't invasive. :)</p>");
+
+        $("#disqus_thread").before(banner_aab, "<hr />");
+    }
+});
